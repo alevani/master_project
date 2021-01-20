64e5c94d146e7b224c5978e4598cacf9a3d1a7ee
alevani <vanini.alexandre@gmail.com>
2021-01-20 15:58:19 +0100

    Simulation: Add env class

---
+++ simulation/env.py
@@ -0,0 +1,4 @@
+# TODO, encapsulate worlds and their global values so be easily used by the simulation. The use of one env to another has to be made easier thanks to this.
+class Environment:
+    def __init__(self):
+        pass
