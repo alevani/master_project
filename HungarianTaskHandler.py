62eb8b4a5c33588468b6f5b22066e267167fd296
alevani <vanini.alexandre@gmail.com>
2021-03-03 11:19:56 +0100

    Hungarian is out of question

--- HungarianTaskHandler.py
+++
@@ -1,9 +0,0 @@
-https: // pypi.org/project/hungarian-algorithm/
-
-I think it will be computionally SUPER HEAVY.
-
-
-at each repor time, the robot would have to
-
-
-if there has to be a task EVERY time then we could have a task where the match would alwyays be - 1000 so it the last thing they choose.
