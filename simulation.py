196dc20636e5c632e8500e6229bc1ba28ed41777
alevani <vanini.alexandre@gmail.com>
2021-03-28 14:31:13 +0200

    Clean add robot list when adding them back as reference was causing problems

--- simulation.py
+++ simulation.py
@@ -563,6 +563,7 @@ while True:
 
             if globals.CNT == 20000:
                 globals.ROBOTS += globals.ADD_AVAILABLE_ROBOTS
+                globals.ADD_AVAILABLE_ROBOTS = []
 
     if ACT:
         pygame .display.flip()  # render drawing
