63af3a65b6e42f6ddb77f4d29e78eac6661a69e3
alevani <vanini.alexandre@gmail.com>
2021-03-30 11:23:04 +0200

    Run file now matches PSI exp requirements

--- run_exps.sh
+++ run_exps.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
-# Lazy me wants to run this on a weekend a do nothing
 
-for e in {1..1}; do
-# for e in {1..3}; do
-    for i in {1..10}; do
-        # for r in `seq 10 10 50`; do
-        python simulation.py -p 2000 -r 50 -s False -b False -t False -a False -f EXP/ALONE/EXP$e/stats25_ATAI_EXP1_R27_RUN$i.csv -e $e
-            # python simulation.py -p 2000_RUNr  -s False -b False -t False -a False -f EXP/ALONE/EXP$e/stats25_ATAI_EXP1_R$r_RUN$i.csv -e $e
-        # done
+
+current_TA="PSI"
+mode=""
+# Exp number
+for e in {1..3}; do
+    mode=""
+    if [[ $e -eq 1 ]]; then
+        mode="æ"
+    elif [[ $e -eq 2 ]]; then
+        mode="@"
+    fi
+
+    
+    # Run number
+    for i in {1..5}; do 
+        if [[ $e -eq 3 ]]; then
+            for m in @ £ ; do
+                python simulation.py -p 2000 -r 40 -s False -b False -t False -a True -n 0.01 -f "${current_TA}/EXP${e}/${m}${current_TA}_r40/RUN${i}.csv" -e $e
+            done
+        else
+            python simulation.py -p 2000 -r 40 -s False -b False -t False -a True -n 0.01 -f "${current_TA}/EXP${e}/${mode}${current_TA}_r40/RUN${i}.csv"  -e $e
+        fi
     done
+    
 done
