1b1cd70a83b766198ea80a68dc5202a5e1da3922
alevani <vanini.alexandre@gmail.com>
2021-03-30 11:30:10 +0200

    Change exp3 steps

--- simulation.py
+++ simulation.py
@@ -546,11 +546,11 @@ while True:
             sys.exit()
 
     elif exp_number == 3:
-        if globals.CNT >= 25000:
+        if globals.CNT >= 60000:
             import sys
             sys.exit()
 
-        if globals.CNT == 10000:
+        if globals.CNT == 20000:
             classes_to_delete = [2]
 
             keep_alive_robot = []
@@ -568,7 +568,7 @@ while True:
 
             globals.ROBOTS = keep_alive_robot
 
-        if globals.CNT == 20000:
+        if globals.CNT == 40000:
             globals.ROBOTS += globals.ADD_AVAILABLE_ROBOTS
             globals.ADD_AVAILABLE_ROBOTS = []
 
