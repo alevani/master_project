6777df232f02e35a6698067602a774c111ea1597
alevani <vanini.alexandre@gmail.com>
2021-04-07 12:29:09 +0200

    .

--- stats/multiple/noise_multiple.py
+++ stats/multiple/noise_multiple.py
@@ -3,20 +3,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 f10 = open(
-    '/Users/freak/Desktop/master_project/stats/0FAITA_EXP_NOISE/EXP1/0.05_æFAITA_r40/0.05_æFAITA_r40.csv')
+    '../0FAITA_EXP_HARD_NOISE/EXP1/0/0.csv')
 f20 = open(
-    '/Users/freak/Desktop/master_project/stats/0FAITA_EXP_NOISE/EXP1/0.1_æFAITA_r40/0.1_æFAITA_r40.csv')
+    '../0FAITA_EXP_HARD_NOISE/EXP1/0.1/0.1.csv')
 f30 = open(
-    '/Users/freak/Desktop/master_project/stats/0FAITA_EXP_NOISE/EXP1/0.3_æFAITA_r40/0.3_æFAITA_r40.csv')
+    '../0FAITA_EXP_HARD_NOISE/EXP1/0.3/0.3.csv')
 f40 = open(
-    '/Users/freak/Desktop/master_project/stats/0FAITA_EXP_NOISE/EXP1/0.5_æFAITA_r40/0.5_æFAITA_r40.csv')
+    '../0FAITA_EXP_HARD_NOISE/EXP1/0.5/0.5.csv')
 f50 = open(
-    '/Users/freak/Desktop/master_project/stats/0FAITA_EXP_NOISE/EXP1/0.7_æFAITA_r40/0.7_æFAITA_r40.csv')
+    '../0FAITA_EXP_HARD_NOISE/EXP1/0.7/0.7.csv')
 f70 = open(
-    '/Users/freak/Desktop/master_project/stats/0FAITA_EXP_NOISE/EXP1/0_æFAITA_r40/0_æFAITA_r40.csv')
+    '../0FAITA_EXP_HARD_NOISE/EXP1/0.99/0.99.csv')
 
 
-step = np.arange(10,  4200, 10)
+step = np.arange(10,  12000, 10)
 
 
 def read(file, shift=0):
@@ -47,27 +47,27 @@ d_70, t_70, n_70 = read(f70, 3)
 
 m = max(t_30[len(t_30) - 1], t_10[len(t_10) - 1],
     t_20[len(t_20) - 1], t_40[len(t_40) - 1], t_50[len(t_50) - 1])
-t_30 += [50 for i in range(len(step) - len(t_30))]
-t_20 += [50 for i in range(len(step) - len(t_20))]
-t_10 += [50 for i in range(len(step) - len(t_10))]
-t_40 += [50 for i in range(len(step) - len(t_40))]
-t_50 += [50 for i in range(len(step) - len(t_50))]
-t_70 += [50 for i in range(len(step) - len(t_70))]
+t_30 += [150 for i in range(len(step) - len(t_30))]
+t_20 += [150 for i in range(len(step) - len(t_20))]
+t_10 += [150 for i in range(len(step) - len(t_10))]
+t_40 += [150 for i in range(len(step) - len(t_40))]
+t_50 += [150 for i in range(len(step) - len(t_50))]
+t_70 += [150 for i in range(len(step) - len(t_70))]
 
 
 fig, total_plot = plt.subplots()
-total_plot.plot(step, [t if not t == None else None for t in t_30],
-                label="P = 0.3")
+total_plot.plot(step, [t if not t == None else None for t in t_10],
+                label="P = 0")
 total_plot.plot(step, [t if not t == None else None for t in t_20],
                 label="P = 0.1")
-total_plot.plot(step, [t if not t == None else None for t in t_10],
-                label="P = 0.05")
+total_plot.plot(step, [t if not t == None else None for t in t_30],
+                label="P = 0.3")
 total_plot.plot(step, [t if not t == None else None for t in t_40],
                 label="P = 0.5")
 total_plot.plot(step, [t if not t == None else None for t in t_50],
                 label="P = 0.7")
 total_plot.plot(step, [t if not t == None else None for t in t_70],
-                label="P = 0")
+                label="P = 0.99")
 
 total_plot.set(xlabel='simulation step', ylabel='Task completion')
 total_plot.grid()
--- stats/multiple/range_multiple.py
+++ stats/multiple/range_multiple.py
@@ -2,20 +2,26 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-f10 = open(
-    '/Users/freak/Desktop/master_project/stats/0FAITA_EXP_RANGE/EXP1/range05/range05.csv')
-f20 = open(
-    '/Users/freak/Desktop/master_project/stats/0FAITA_EXP_RANGE/EXP1/range1/range1.csv')
-f30 = open(
-    '/Users/freak/Desktop/master_project/stats/0FAITA_EXP_RANGE/EXP1/range3/range3.csv')
-f40 = open(
-    '/Users/freak/Desktop/master_project/stats/0FAITA_EXP_RANGE/EXP1/range5/range5.csv')
-f50 = open(
-    '/Users/freak/Desktop/master_project/stats/0FAITA_EXP_RANGE/EXP1/range7/range7.csv')
-f70 = open(
-    '/Users/freak/Desktop/master_project/stats/0FAITA_EXP_NOISE/EXP1/0_æFAITA_r40/0_æFAITA_r40.csv')
 
-step = np.arange(10,  6000, 10)
+f0 = open(
+    '../0FAITA_EXP_HARD_RANGE/EXP1/range0.1/range0.1.csv')
+f10 = open(
+    '../0FAITA_EXP_HARD_RANGE/EXP1/range0.5/range0.5.csv')
+f20 = open(
+    '../0FAITA_EXP_HARD_RANGE/EXP1/range1/range1.csv')
+f30 = open(
+    '../0FAITA_EXP_HARD_RANGE/EXP1/range5/range5.csv')
+f40 = open(
+    '../0FAITA_EXP_HARD_RANGE/EXP1/range7/range7.csv')
+f50 = open(
+    '../0FAITA_EXP_HARD_RANGE/EXP1/range13/range13.csv')
+f70 = open(
+    '../0FAITA_EXP_HARD_RANGE/EXP1/range15/range15.csv')
+f8000 = open(
+    '../0FAITA_EXP_HARD_RANGE/EXP1/range80000/range80000.csv')
+
+
+step = np.arange(10,  12000, 10)
 
 
 def read(file, shift=0):
@@ -42,32 +48,43 @@ d_10, t_10, n_10 = read(f10, 3)
 d_40, t_40, n_40 = read(f40, 3)
 d_50, t_50, n_50 = read(f50, 3)
 d_70, t_70, n_70 = read(f70, 3)
+d_0, t_0, n_0 = read(f0, 3)
+d_8000, t_8000, n_8000 = read(f8000, 3)
 
 
 
 # m = max(t_30[len(t_30) - 1], t_10[len(t_10) - 1],
 #         t_20[len(t_20) - 1], t_40[len(t_40) - 1], t_50[len(t_50) - 1])
-t_30 += [50 for i in range(len(step) - len(t_30))]
-t_20 += [50 for i in range(len(step) - len(t_20))]
-t_10 += [50 for i in range(len(step) - len(t_10))]
-t_40 += [50 for i in range(len(step) - len(t_40))]
-t_50 += [50 for i in range(len(step) - len(t_50))]
-t_70 += [50 for i in range(len(step) - len(t_70))]
+t_30 += [150 for i in range(len(step) - len(t_30))]
+t_20 += [150 for i in range(len(step) - len(t_20))]
+t_10 += [150 for i in range(len(step) - len(t_10))]
+t_40 += [150 for i in range(len(step) - len(t_40))]
+t_50 += [150 for i in range(len(step) - len(t_50))]
+t_70 += [150 for i in range(len(step) - len(t_70))]
+t_0 += [150 for i in range(len(step) - len(t_0))]
+t_8000 += [150 for i in range(len(step) - len(t_8000))]
 
 
 fig, total_plot = plt.subplots()
-total_plot.plot(step, [t if not t == None else None for t in t_30],
-                label="3m")
-total_plot.plot(step, [t if not t == None else None for t in t_20],
-                label="1m")
+# total_plot.plot(step, [t if not t == None else None for t in t_0],
+#                 label="0.1m")
 total_plot.plot(step, [t if not t == None else None for t in t_10],
                 label="0.5m")
+total_plot.plot(step, [t if not t == None else None for t in t_20],
+                label="1m")
+total_plot.plot(step, [t if not t == None else None for t in t_30],
+                label="5m")
 total_plot.plot(step, [t if not t == None else None for t in t_40],
-                label="5m ")
+                label="7m ")
+
 total_plot.plot(step, [t if not t == None else None for t in t_50],
-                label="7m")
+                label="13m")
 total_plot.plot(step, [t if not t == None else None for t in t_70],
+                label="15m")
+total_plot.plot(step, [t if not t == None else None for t in t_8000],
                 label="Infinite range")
+
+
 
 total_plot.set(xlabel='simulation step', ylabel='Task completion')
 total_plot.grid()
